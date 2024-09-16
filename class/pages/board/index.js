9472a91a74111327a4827987fe94526f72d70e9c
oet <dhdmsxor1@cj.net>
2024-09-16 22:24:12 +0900

    next js 프로젝트 생성

---
+++ class/pages/board/index.js
@@ -0,0 +1,12 @@
+export default function board () {
+  // 위쪽은 자바스크립트 작성하는 공간
+  const aaa = 3; 
+
+  // 아래쪽은 html 작성하는 공간
+  return (
+    <div>
+      <div>철수</div>
+      <button>클릭하세요!!</button>
+    </div>
+  );
+}
